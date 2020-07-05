f75d6891c85f35ac44efb5d47ae0a4b18562483e
Jakub Levý <jakub.levy15@gmail.com>
2020-07-05 14:12:33 +0200

    frontend project added

---
+++ frontend/src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../css/App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const App = () => {
+  return (
+      <>
+          <header>
+              <h1>Puppeteer recorder</h1>
+              <hr/>
+          </header>
+      </>
+  )
+}
+
+
+export default App;
