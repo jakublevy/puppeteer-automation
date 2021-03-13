a6f9ed8dfb07cb37ed3c692220c2be554f39d6b9
Jakub Levý <jakub.levy15@gmail.com>
2021-03-13 09:58:52 +0100

    Node.js scripts improved

--- Scripts/node.js-install.bat
+++ Scripts/node.js-install.bat
@@ -1,54 +1,76 @@
 @echo off
 
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+	echo Administrative permission required, please run %0 again as an administrator.
+	goto :exit_handler
+)
+
+set "olddir=%cd%"
+cd /d "%~dp0"
+
 if exist "%programfiles(x86)%" (
-	:: 64-bit
+	rem 64-bit
 	set "prodcode={179B1CCA-65E3-4A97-8208-590C0160E347}"
 	set "nodejsurl=https://nodejs.org/download/release/v15.5.0/node-v15.5.0-x64.msi"
 ) else (
-	:: 32-bit
+	rem 32-bit
 	set "prodcode={7136155B-06A7-43C8-BB15-C01141CBBB8C}"
 	set "nodejsurl=https://nodejs.org/download/release/v15.5.0/node-v15.5.0-x86.msi"
 )
 
-:: replace / with space
+rem replace / with space
 for %%a in (%nodejsurl:/= %) do set "nodejsfile=%%a"
 
-:: check if installed
+rem check if installed
 reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" | findstr /I /C:"%prodcode%" >nul
 
-if %ERRORLEVEL% equ 0 (
-	:: already installed, exiting
+if %errorlevel% equ 0 (
+	rem already installed, exiting
 	echo %nodejsfile% with GUID: %prodcode% is installed.
-	exit /b
+	goto :exit_handler
 )
 
 set "created=0"
-:: download suitable node.js installer
+rem download suitable node.js installer
 if not exist "%~dp0%nodejsfile%" (
 	set "created=1"
 	bitsadmin /transfer "Downloading %nodejsfile%" "%nodejsurl%" "%~dp0%nodejsfile%"
 )
 
-:: installation file already exists
+rem installation file already exists
 if %created% equ 0 (
 	echo Found existing %nodejsfile%, will be used for installation.
 )
 echo Installing... Please wait...
-:: install it on the background
+rem install it on the background
 %nodejsfile% /quiet /qn /norestart
 
-if %ERRORLEVEL% equ 0 (
+if %errorlevel% equ 0 (
 	echo Installation done.
 ) else (
 	echo Installation failed.
 	if %created% equ 0 (
-		:: installation failed with an pre-existing installer
+		rem installation failed with an pre-existing installer
 		echo Incorrect existing %nodejsfile%. Remove it and run %0 again.
 	)
 )
 
-:: remove downloaded installer
+rem remove downloaded installer
 if %created% equ 1 (
 	del /Q /F "%~dp0%nodejsfile%"
 	echo Downloaded installer file "%nodejsfile%" removed.
 )
+
+cd /d "%olddir%"
+
+goto :exit_handler
+
+:exit_handler
+	rem Launched using double-click?
+	echo %cmdcmdline% | findstr /I /C:/c >nul
+	if %errorlevel% equ 0 (
+		rem double-clicked
+		pause
+	)
+exit /b
--- Scripts/node.js-uninstall.bat
+++ Scripts/node.js-uninstall.bat
@@ -1,23 +1,40 @@
 @echo off
 
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+	echo Administrative permission required, please run %0 again as an administrator.
+	goto :exit_handler
+)
+
 if exist "%programfiles(x86)%" (
-	:: 64-bit
+	rem 64-bit
 	set "prodcode={179B1CCA-65E3-4A97-8208-590C0160E347}"
 	set "nodejsfile=node-v15.5.0-x64.msi"
 ) else (
-	:: 32-bit
+	rem 32-bit
 	set "prodcode={7136155B-06A7-43C8-BB15-C01141CBBB8C}"
 	set "nodejsfile=node-v15.5.0-x86.msi"
 )
 
-:: check if installed
+rem check if installed
 reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" | findstr /I /C:"%prodcode%" >nul
-if %ERRORLEVEL% equ 0 (
-	:: it is installed
+if %errorLevel% equ 0 (
+	rem it is installed
 	echo Uninstalling... Please wait...
-	:: uninstall it on the background
+	rem uninstall it on the background
 	msiexec /uninstall "%prodcode%" /quiet /qn /norestart
 	echo Uninstallation done.
 ) else (
 	echo %nodejsfile% with GUID: %prodcode% is not installed.
 )
+
+goto :exit_handler
+
+:exit_handler
+	rem Launched using double-click?
+	echo %cmdcmdline% | findstr /I /C:/c >nul
+	if %errorlevel% equ 0 (
+		rem double-clicked
+		pause
+	)
+exit /b
